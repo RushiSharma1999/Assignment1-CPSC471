c6dabf2357d251236bffa59e66a2c6e3d55a2b98
jssaari <31286755+jssaari@users.noreply.github.com>
2020-05-02 21:59:52 -0700

    Update ftp-client.py

--- ftp-client.py
+++ ftp-client.py
@@ -3,18 +3,18 @@
 from ftplib import FTP
 
 ftp = FTP('')
-ftp.connect('IPaddress',1026) #use the IP Address you're trying to reach
+ftp.connect('IPaddress',1026) #the IP Address you're trying to reach
 ftp.login()
-ftp.cwd('/home/directory') #replace with your directory
+ftp.cwd('/home') #your directory
 ftp.retrlines('LIST')
 
 def uploadFile():
- filename = 'ftptest.txt' #replace with your file in your home folder
+ filename = 'ftptest.txt' #your file in your home folder
  ftp.storbinary('STOR '+filename, open(filename, 'rb'))
  ftp.quit()
 
 def downloadFile():
- filename = 'dltest.txt' #replace with your file in the directory ('directory_name')
+ filename = 'dltest.txt' #your file in the directory ('directory_name')
  localfile = open(filename, 'wb')
  ftp.retrbinary('RETR ' + filename, localfile.write, 1024)
  ftp.quit()
