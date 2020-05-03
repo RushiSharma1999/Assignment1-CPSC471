76f2a289715d6dcda84dd28688d8cd25d9679841
jssaari <31286755+jssaari@users.noreply.github.com>
2020-05-02 21:55:53 -0700

    Server side ftp using ftp library

---
+++ ftp-server.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+authorizer = DummyAuthorizer()
+authorizer.add_user("user", "Passwd", "/home/directory", perm="elradfmw") #user, pass, directory
+authorizer.add_anonymous("/home/directory", perm="elradfmw") #enter directory
+
+handler = FTPHandler
+handler.authorizer = authorizer
+
+server = FTPServer(("IPaddress", 1026), handler) #IP Address
+server.serve_forever()
