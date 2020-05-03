1d96a575380f90df324e6f7cf5a007ab5a8d4668
jssaari <31286755+jssaari@users.noreply.github.com>
2020-05-02 22:00:43 -0700

    Update ftp-server.py

--- ftp-server.py
+++ ftp-server.py
@@ -6,10 +6,10 @@ from pyftpdlib.servers import FTPServer
 
 authorizer = DummyAuthorizer()
 authorizer.add_user("user", "Passwd", "/home/directory", perm="elradfmw") #user, pass, directory
-authorizer.add_anonymous("/home/directory", perm="elradfmw") #enter directory
+authorizer.add_anonymous("/home", perm="elradfmw") #enter directory
 
 handler = FTPHandler
 handler.authorizer = authorizer
 
-server = FTPServer(("IPaddress", 1026), handler) #IP Address
+server = FTPServer(("127.0.0.1", 1026), handler) #IP Address
 server.serve_forever()
